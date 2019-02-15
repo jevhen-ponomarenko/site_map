ee2d2bfa393f4ec661b3e03b41cbf3114fe27043
ponomyev <ponomyev@fel.cvut.cz>
2019-02-15 16:03:15 +0100

    writing tests

--- parser.py
+++ parser.py
@@ -6,15 +6,21 @@ def parse_file(path):
     """:returns dictionary {0 : dict, 1 : dict, ...}"""
     counter = 0
     line_num_to_tokens_dict = {}
-    file = open(path, 'r')
 
-    for line in file:
-        if(line == ''):
-            break
+    try:
+        file = open(path, 'r')
+        for line in file:
+            if (line == ''):
+                break
 
-        line_num_to_tokens_dict[counter] = tokenize_line(line)
-        counter += 1
-    file.close()
-    return line_num_to_tokens_dict
+            line_num_to_tokens_dict[counter] = tokenize_line(line)
+            counter += 1
+        file.close()
+        return line_num_to_tokens_dict
+
+    except FileNotFoundError:
+        print('{0} File not Found'.format(path))
+
+
 
 
